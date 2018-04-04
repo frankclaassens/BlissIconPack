316b97f46ae13a021fc16feda1c1f2d47ea1e8c3
Romain Hunault <romain.hunault@eelo.io>
2018-04-04 10:05:11 +0200

    Add assets directory

--- Android.mk
+++ Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MANIFEST_FILE := app/src/main/AndroidManifest.xml
 LOCAL_PACKAGE_NAME := BlissIconPack
+LOCAL_ASSET_DIR += $(LOCAL_PATH)/app/src/main/assets/
 LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/app/src/main/res/
 include $(BUILD_PACKAGE)
 include $(call all-makefiles-under,$(LOCAL_PATH))
